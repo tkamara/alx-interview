b72e9ad40a1705f94f2bfb71ea22d961f0685ab9
Chebet Kamara <chebetkamara@gmail.com>
2023-10-12 15:47:39 +0000

    Minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Minimum operations
+"""
+
+
+def minOperations(n):
+    """
+    calculates the fewest number of operations needed to
+    result in exactly n H characters in the file
+    """
+    if n <= 1:
+        return 0
+
+    operations = 0
+    copy = 0
+    complete = 1
+
+    while complete < n:
+        if copy == 0 or (n % complete) == 0:
+            copy = complete
+            complete += copy
+            operations += 2
+        elif copy > 0:
+            complete += copy
+            operations += 1
+
+    return operations
