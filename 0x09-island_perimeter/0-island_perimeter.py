0034e7334fc79e14eda4dda91b681196575411ed
Chebet Kamara <chebetkamara@gmail.com>
2023-11-30 10:48:54 -0800

    Island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,20 @@
+def island_perimeter(grid):
+    if not grid or not grid[0]:
+        return 0
+
+    row = len(grid)
+    col = len(grid[0])
+    perimeter = 0
+
+    for i in range(row):
+        for j in range(col):
+            if grid[i][j] == 1:
+                if i == 0 or grid[i - 1][j] == 0:
+                    perimeter += 1
+                if j == 0 or grid[i][j - 1] == 0:
+                    perimeter += 1
+                if i == row - 1 or grid[i + 1][j] == 0:
+                    perimeter += 1
+                if j == col - 1 or grid[i][j + 1] == 0:
+                    perimeter += 1
+    return perimeter
